59a642ef4a8928a077c29b2bffbe8887813c7a96
Alexandre GARCIN <perso@alex-home.fr>
2024-12-31 22:00:18 +0100

    base

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,34 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 import { libInjectCss } from 'vite-plugin-lib-inject-css'
 import eslint from 'vite-plugin-eslint';
 import path from "path";
 import sass from 'vite-plugin-sass';
+import externalGlobals from 'rollup-plugin-external-globals';
+import { minify } from "terser";
+
+import { peerDependencies } from "./package.json";
+
+const minifyBundles = () => {
+   return {
+      name: "minifyBundles",
+      async generateBundle(options, bundle) {
+         for (const key in bundle) {
+            if (bundle[key].type === 'chunk' && key.endsWith('.js')) {
+               const minifyCode = await minify(bundle[key].code, { sourceMap: false })
+               bundle[key].code = `(()=>{${minifyCode.code}})()`
+            }
+         }
+         return bundle
+      },
+   }
+}
 
 export default defineConfig({
    define: {
       BASE_URL: JSON.stringify(`coui://html_ui/efb_ui/efb_apps/msfs2024-vfrnav`)
    },
    build: {
-      minify: true,
+      minify: false,
       lib: {
          entry: "./src/App.tsx",
          fileName: () => "App.js",
@@ -20,6 +39,7 @@ export default defineConfig({
          output: {
             manualChunks: undefined,
          },
+         external: [...Object.keys(peerDependencies)],
       },
       ssr: false,
       sourcemap: true,
@@ -35,5 +55,10 @@ export default defineConfig({
       eslint(),
       sass(),
       libInjectCss(),
+      externalGlobals({
+         "@microsoft/msfs-sdk": "msfssdk",
+         "@workingtitlesim/garminsdk": "garminsdk"
+      }),
+      minifyBundles()
    ],
 });
