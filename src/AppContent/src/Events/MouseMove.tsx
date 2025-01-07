8ae51a1148f4bda4070123d662f812e8fedbf6a6
Alexandre GARCIN <perso@alex-home.fr>
2025-01-08 00:39:04 +0100

    Buggy Scroll #20

--- src/AppContent/src/Events/MouseMove.tsx
+++ src/AppContent/src/Events/MouseMove.tsx
@@ -1,18 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export default function useMouseMove(active?: boolean) {
    const [mousePosition, setMousePosition] = useState<{ x: number, y: number } | undefined>();
+   const result = useMemo(() => (active ?? true) ? mousePosition : undefined, [active, mousePosition]);
 
    useEffect(() => {
       const handleMouseMove = (e: MouseEvent) => {
-         if (active ?? true) {
-            setMousePosition({ x: e.clientX, y: e.clientY });
-         }
+         setMousePosition({ x: e.clientX, y: e.clientY });
       };
 
       document.addEventListener('mousemove', handleMouseMove);
       return () => document.removeEventListener('mousemove', handleMouseMove);
-   }, [active]);
+   }, []);
 
-   return (active ?? true) ? mousePosition : undefined;
+   return result;
 }
--- src/AppContent/src/Utils/Scroll.tsx
+++ src/AppContent/src/Utils/Scroll.tsx
@@ -1,36 +1,43 @@
+import useMouseMove from '@/Events/MouseMove';
 import useMouseRelease from '@/Events/MouseRelease';
-import { PropsWithChildren, useRef, useEffect, useState, CSSProperties, useCallback, useMemo } from 'react';
-import useMouseDrag from '@/Events/MouseDrag';
+import { PropsWithChildren, useRef, useEffect, useState, CSSProperties, useCallback, MouseEvent } from 'react';
 
 export const Scroll = ({ children, className, style }: PropsWithChildren<{
    className?: string,
    style?: CSSProperties
 }>) => {
    const [scroll, setScroll] = useState({ x: 0, y: 0, ratioX: 0, ratioY: 0, width: 0, height: 0, displayX: false, displayY: false });
-   const [lastPos, setLastPos] = useState<{ x: number, y: number }>();
+   const [scrolling, setScrolling] = useState<{ vertical: boolean } | undefined>(undefined);
    const [visible, setVisible] = useState(0);
    const [selectable, setSelectable] = useState(true);
-   const useDrag = useMemo(() => lastPos !== undefined, [lastPos]);
-   const mousePosition = useMouseDrag(useDrag);
+   const mousePosition = useMouseMove(scrolling !== undefined);
+   const [offset, setOffset] = useState(0);
    const mouseUp = useMouseRelease();
    const ref = useRef<HTMLDivElement | null>(null);
 
    useEffect(() => {
       if (mouseUp !== undefined) {
+         setScrolling(undefined);
          setSelectable(true);
-         setLastPos(undefined);
+         setOffset(0);
          setTimeout(() => {
             setVisible(count => count === visible ? 0 : count);
          }, 1000);
       }
-   }, [mouseUp, visible]);
+   }, [mouseUp, scrolling, visible]);
 
    useEffect(() => {
-      if (mousePosition && lastPos && ref.current) {
-         ref.current.scroll(ref.current.scrollLeft + (mousePosition.x - lastPos.x) * scroll.ratioX, ref.current.scrollTop + (mousePosition.y - lastPos.y) * scroll.ratioY);
-         setLastPos(mousePosition);
+      if (mousePosition && scrolling && ref.current) {
+         const boundings = ref.current?.parentElement!.getBoundingClientRect();
+
+         if (scrolling.vertical) {
+            ref.current?.scroll({ top: (mousePosition.y + offset - (boundings?.top ?? 0) - scroll.height / 2) * scroll.ratioY });
+         } else {
+            console.log((mousePosition.x - (boundings?.left ?? 0) - scroll.width / 2) * scroll.ratioX)
+            ref.current?.scroll({ left: (mousePosition.x + offset - (boundings?.left ?? 0) - scroll.width / 2) * scroll.ratioX });
+         }
       }
-   }, [mousePosition, lastPos, setLastPos, scroll]);
+   }, [mousePosition, scroll.height, scroll.ratioX, scroll.ratioY, scroll.width, scrolling, offset]);
 
    const updateScroll = useCallback((target: HTMLDivElement) => {
       setVisible(visible => visible + 1);
@@ -46,13 +53,28 @@ export const Scroll = ({ children, className, style }: PropsWithChildren<{
       setScroll({ x: moveSize[0] * posRatio[0], y: moveSize[1] * posRatio[1], ratioX: outer[0] / moveSize[0], ratioY: outer[1] / moveSize[1], width: scrollSize[0], height: scrollSize[1], displayX: outer[0] !== 0, displayY: outer[1] !== 0 })
    }, [setScroll]);
 
+   const onClick = useCallback((_e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>, vertical: boolean) => {
+      setSelectable(false);
+      setScrolling({ vertical: vertical });
+   }, []);
+
+   const onScrollClick = useCallback((e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>, vertical: boolean) => {
+      const boundings = ref.current?.parentElement!.getBoundingClientRect();
+
+      if (vertical) {
+         setOffset((scroll.height / 2 + scroll.y) - (e.clientY - (boundings?.top ?? 0)));
+      } else {
+         setOffset((scroll.width / 2 + scroll.x) - (e.clientX - (boundings?.left ?? 0)));
+      }
+   }, [scroll.height, scroll.width, scroll.x, scroll.y]);
+
    useEffect(() => {
       if (visible) {
          setTimeout(() => {
-            setVisible(count => count === visible && lastPos === undefined ? 0 : count);
+            setVisible(count => count === visible && scrolling === undefined ? 0 : count);
          }, 1000);
       }
-   }, [visible, lastPos]);
+   }, [visible, scrolling]);
 
    useEffect(() => {
       if (ref.current) {
@@ -61,27 +83,35 @@ export const Scroll = ({ children, className, style }: PropsWithChildren<{
    }, [updateScroll, ref.current?.scrollWidth, ref.current?.scrollHeight, ref.current?.scrollTop, ref.current?.scrollLeft]);
 
    return <div className='flex relative overflow-hidden'>
-      <div className={'group absolute transition duration-1000 hover:duration-200 flex flex-row right-0 top-0 w-[7px] bottom-0 bg-gray-800 bg-opacity-50 opacity-0 hover:opacity-100'
+      {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events */}
+      <div className={'group absolute transition duration-1000 hover:duration-200 hover:cursor-pointer'
+         + ' flex flex-row right-0 top-0 w-[7px] bottom-0 bg-gray-800 bg-opacity-50 opacity-0 hover:opacity-100'
          + (visible ? ' opacity-100' : '')
          + (scroll.displayY ? '' : ' hidden')
-      }>
+      }
+         onMouseDown={(e) => onClick(e, true)}
+      >
          {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
          <div className={'transition grow bg-gray-700 group-hover:bg-msfs'
             + (visible ? ' bg-msfs' : '')} style={{ marginTop: scroll.y, height: scroll.height }}
-            onMouseDown={e => {
-               setSelectable(false);
-               setLastPos({ x: e.clientX, y: e.clientY });
-            }}
-            onMouseUp={() => {
-               setSelectable(true);
-               setLastPos(undefined);
-            }}
-         ></div>
+            onMouseDown={e => onScrollClick(e, true)}
+         />
       </div>
-      <div className={'group absolute right-0 bottom-0 h-2 bg-gray-700 group-hover:bg-msfs'
+      {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events */}
+      <div className={'group absolute transition duration-1000 hover:duration-200 hover:cursor-pointer'
+         + ' flex flex-col left-0 right-0 h-[7px] bottom-0 bg-gray-800 bg-opacity-50 opacity-0 hover:opacity-100'
+         + (visible ? ' opacity-100' : '')
          + (scroll.displayX ? '' : ' hidden')
-      } style={{ left: scroll.x, width: scroll.width }}></div>
-      <div className={'box-content overflow-y-scroll [scrollbar-width:none] '
+      }
+         onMouseDown={(e) => onClick(e, false)}
+      >
+         {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
+         <div className={'transition grow bg-gray-700 group-hover:bg-msfs'
+            + (visible ? ' bg-msfs' : '')} style={{ marginLeft: scroll.x, width: scroll.width }}
+            onMouseDown={e => onScrollClick(e, false)}
+         />
+      </div>
+      <div className={'box-content overflow-scroll [scrollbar-width:none] '
          + (className ?? '')
          + (selectable ? '' : ' select-none')}
          style={style}
