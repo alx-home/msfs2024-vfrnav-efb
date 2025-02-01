3df27409316a59c5489fb49cf90303ad92f71ece
Alexandre GARCIN <perso@alex-home.fr>
2025-02-01 02:38:36 +0100

    restore minify

--- src/AppContent/build.ts
+++ src/AppContent/build.ts
@@ -12,7 +12,7 @@ import autoprefixer from "autoprefixer";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const minify = false;
+const minify = true;
 
 const msfsEmbeded = process.env.MSFS_EMBEDED;
 console.assert(msfsEmbeded !== undefined)
