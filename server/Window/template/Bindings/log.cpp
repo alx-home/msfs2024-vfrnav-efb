ea1a687bab729a2b6c01e09ab07f274b64ae49cf
Alexandre GARCIN <perso@alex-home.fr>
2025-06-15 17:58:02 +0200

    Update License

--- server/Window/template/Bindings/log.cpp
+++ server/Window/template/Bindings/log.cpp
@@ -1,1 +1,16 @@
+/*
+ * SPDX-License-Identifier: (GNU General Public License v3.0 only)
+ * Copyright © 2024 Alexandre GARCIN
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <https://www.gnu.org/licenses/>.
+ */
+
 // Hack for compile commands
