b041b05118953b084728f7d572a53e4af5101e5c
Alexandre GARCIN <perso@alex-home.fr>
2025-07-03 18:46:45 +0200

    Fix promise segfault

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -9,6 +9,7 @@ endif()
 # Promise
 option(PROMISE_MEMCHECK_RELEASE "Enable promise leak detection in release mode" OFF)
 option(PROMISE_MEMCHECK_DEBUG "Enable promise leak detection in debug mode" ON)
+option(PROMISE_MEMCHECK_FULL "Dump leaked promises" ON)
 
 # Webview
 if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
